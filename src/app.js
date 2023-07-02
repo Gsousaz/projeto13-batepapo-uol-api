c9867a5217050203d5a8f2f55616210c624e08a5
Gsousaz <doomlighthighlord@gmail.com>
2023-07-02 18:17:14 -0300

    criada rota post /status

--- src/app.js
+++ src/app.js
@@ -28,7 +28,6 @@ app.post("/participants", async (req, res) => {
 
   const schemaParticipant = joi.object({
     name: joi.string().required(),
-    lastStatus: joi.date().timestamp('javascript').required(),
   }); // => Define o esquema de validação do campo name.
 
   const validation = schemaParticipant.validate(req.body, {
@@ -40,8 +39,6 @@ app.post("/participants", async (req, res) => {
     return res.status(422).send(errors); // => Retorna para o usuário os erros que porventura venham a ocorrer.
   }
 
-  if (!name) return res.status(422).send("Preencha todos os campos"); //Verifica se todos os campos foram preenchidos antes de fazer a requisição
-
   try {
     const participant = await db
       .collection("participants")
@@ -50,7 +47,7 @@ app.post("/participants", async (req, res) => {
 
     await db
       .collection("participants")
-      .insertOne({ name: name, lastStatus: Date.now() }); // => Inserção do usuário no banco de dados
+      .insertOne({ name: name, lastStatus: Date.now()}); // => Inserção do usuário no banco de dados
 
     await db.collection("messages").insertOne({
       from: name,
@@ -93,13 +90,13 @@ app.post("/messages", async (req, res) => {
   });
 
   if (validation.error) {
-    return res.sendStatus(422) // => Alteração: Tentando enviar apenas o status por conta de erro na avaliação automática.
+    return res.sendStatus(422); // => Alteração: Tentando enviar apenas o status por conta de erro na avaliação automática.
   }
 
   try {
     const participant = await db
       .collection("participants")
-      .findOne({ name: req.headers.user });
+      .findOne({ name: user });
     if (!participant) return res.status(401).send("Permissão Negada");
 
     await db.collection("messages").insertOne({
@@ -119,27 +116,79 @@ app.post("/messages", async (req, res) => {
 
 app.get("/messages", async (req, res) => {
   const user = req.headers.user;
-  const mensagensFiltradas = {$or: [{from: user},{to: user}, {to: "Todos"},{type: "message"},]}
+  const mensagensFiltradas = {
+    $or: [{ from: user }, { to: user }, { to: "Todos" }, { type: "message" }],
+  };
   const limit = req.query.limit;
   let messages;
 
   try {
-    if (limit === undefined) { // => Caso não seja informado um limite vamos mostrar todas as mensagens que o usuário pode ver.
-      messages = await db.collection("messages").find(mensagensFiltradas).toArray();
-  } else if (isNaN(limit) || limit <= 0){ // => Caso seja informado um limite não numérico ou menor/igual a 0, retornamos o status 422.
-    res.sendStatus(422)
-  } else {
-    messages = await db.collection("messages").find(mensagensFiltradas).limit(parseInt(limit)).toArray(); // => Caso o limite informado 
-    // seja um número válido, mostramos a quantidade de mensagens solicitada para ele \o/
-  }
+    if (limit === undefined) {
+      // => Caso não seja informado um limite vamos mostrar todas as mensagens que o usuário pode ver.
+      messages = await db
+        .collection("messages")
+        .find(mensagensFiltradas)
+        .toArray();
+    } else if (isNaN(limit) || limit <= 0) {
+      // => Caso seja informado um limite não numérico ou menor/igual a 0, retornamos o status 422.
+      res.sendStatus(422);
+    } else {
+      messages = await db
+        .collection("messages")
+        .find(mensagensFiltradas)
+        .limit(parseInt(limit))
+        .toArray(); // => Caso o limite informado
+      // seja um número válido, mostramos a quantidade de mensagens solicitada para ele \o/
+    }
 
-    res.status(200).send(messages)
+    res.status(200).send(messages);
   } catch (err) {
     res.status(500).send(err.message);
   }
-  
+});
+
+//-----------------------INICIO DA ROTA POST "/STATUS"-----------------------//
+
+app.post("/status", async (req, res) => {
+  const user = req.headers.user;
+
+  try {
+    if (!user) {
+      res.sendStatus(404);
+    }
+    
+    const participant = await db.collection("participants").findOne({ name: user });
+    if (!participant) return res.sendStatus(404);
+
+    db.collection("participants").updateOne({name: user},{$set: {lastStatus: Date.now()}}, { upsert: true });
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 
+
+
+app.delete("/messages", async (req, res) => {
+  try {
+    await db.collection("messages").deleteMany({});
+    res.sendStatus(200);
+  } catch (err) {
+    res.sendStatus(400);
+  }
+});
+
+
+  app.delete("/participants", async(req,res) =>{
+    try{
+      await db.collection("participants").deleteMany({});
+      res.sendStatus(200);
+    } catch (err){
+      res.sendStatus(400);
+    }
+})
+
+
+
 app.listen(PORT, () => console.log(`O servidor está rodando na porta ${PORT}`));
-   
